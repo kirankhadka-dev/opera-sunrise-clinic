4648e42ba2697f87daad766188cb8b4852bbf456
Kiran Khadka <kiran.khadka.dev@gmail.com>
2025-02-23 17:56:04 +1100

    Add generate refresh token and access token

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
       index: true,
     },
 
-    fullName: { type: String, required: true, trim: true },
+    fullname: { type: String, required: true, trim: true },
 
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
@@ -35,6 +35,39 @@ userSchema.pre(
     next();
   })
 );
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return bcrypt.compare(password, this);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      role: this.role,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      role: this.role,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 const UserModel = mongoose.model("User", userSchema);
 export default UserModel;
